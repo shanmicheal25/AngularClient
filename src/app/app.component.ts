bf7fd8e409c2b1c7236106a6375d06560d151a98
Shankar Manoharan <shankar.manoharan@singtel.com>
2020-07-06 10:07:40 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import { TabsComponent } from './tabs/tabs.component';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularClient';
+  @ViewChild('personEdit', {static: true}) editPersonTemplate;
+  @ViewChild('about', {static: true}) aboutTemplate;
+  @ViewChild(TabsComponent, {static: true}) tabsComponent;
+
+  people = [
+    {
+      id: 1,
+      name: 'Juri',
+      surname: 'Strumpflohner',
+      twitter: '@juristr'
+    }
+  ];
+
+  onEditPerson(person) {
+    this.tabsComponent.openTab(
+      `Editing ${person.name}`,
+      this.editPersonTemplate,
+      person,
+      true
+    );
+  }
+
+  onAddPerson() {
+    this.tabsComponent.openTab('New Person', this.editPersonTemplate, {}, true);
+  }
+
+  onPersonFormSubmit(dataModel) {
+    if (dataModel.id > 0) {
+      this.people = this.people.map(person => {
+        if (person.id === dataModel.id) {
+          return dataModel;
+        } else {
+          return person;
+        }
+      });
+    } else {
+      // create a new one
+      dataModel.id = Math.round(Math.random() * 100);
+      this.people.push(dataModel);
+    }
+
+    // close the tab
+    this.tabsComponent.closeActiveTab();
+  }
+
+  onOpenAbout() {
+    this.tabsComponent.openTab('About', this.aboutTemplate, {}, true);
+  }
 }
